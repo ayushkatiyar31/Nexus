db1e8d087f179be7d043b3dafe5e9a8e2e6cb739
Ayush Katiyar <ayushkatiyar827@gmail.com>
2025-10-01 18:12:32 +0530

    make changes

--- 02React/DAY4/myntra.js
+++ 02React/DAY4/myntra.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
- 
+
 
 // Header
 // Body
@@ -19,6 +19,49 @@ function Card(props){
     )
 }
 
+const arr = [{cloth:"Tshirt", Offer:"20-40%Off"},{cloth:"Pant", Offer:"30-50%Off"},{cloth:"Skirt", Offer:"10-20%Off"},{cloth:"Kurta", Offer:"30-60%Off"},{cloth:"Patloon", Offer:"11-40%Off"},{cloth:"Shoes", Offer:"40-60%Off"},{cloth:"Shirt", Offer:"10-20%Off"}]
+
+
+
+function App(){
+  return(
+     
+    // Header
+    // Body
+    <div style={{display:"flex", gap:"10px" , flexWrap:"wrap"}}>
+        <div className="heading">
+        
+        <img className="images" src="https://images.indianexpress.com/2021/01/myntra.png" height="80px" width="80px"/>
+        <div className="option">
+          <button className="but">Men</button>
+          <button className="but">Women</button>
+          <button className="but">Kids</button>
+          <button className="but">Home and Living</button>
+          <button className="but">Beauty</button>
+          <button className="but">Studio</button>
+        </div>
+  
+        <input className="searchbar" placeholder="Search for products brands and more"></input>
+  
+        <div className="Profile">
+          <button className="pro">Profiles</button>
+          <button className="pro">Wishlist</button>
+          <button className="pro">Bag</button>
+        </div>
+      </div>
+    )
+        {
+          arr.map((value,index)=> <Card key={index} cloth={value.cloth} offer={value.Offer}/>)   
+        }
+       
+    </div>
+
+
+    // footer
+  )
+}
+
+// [<Card/>,<Card/>,<Card/>,<Card/>,<Card/>,<Card/>]
 
 
 const Root = ReactDOM.createRoot(document.getElementById('root'));
